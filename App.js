f97d395aa9dd8a27af83af3e4179b8606915a449
Jared Forsyth <jared@jaredforsyth.com>
2022-12-14 16:52:21 -0600

    ok, minimal repro

--- App.js
+++ App.js
@@ -1,117 +1,74 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- * @flow strict-local
- */
-
-import React from 'react';
+import {createNativeStackNavigator} from '@react-navigation/native-stack';
 import type {Node} from 'react';
-import {
-  SafeAreaView,
-  ScrollView,
-  StatusBar,
-  StyleSheet,
-  Text,
-  useColorScheme,
-  View,
-} from 'react-native';
+import React, {useRef, useState} from 'react';
+import {Button, Text, View} from 'react-native';
+import Video from 'react-native-video';
 
-import {
-  Colors,
-  DebugInstructions,
-  Header,
-  LearnMoreLinks,
-  ReloadInstructions,
-} from 'react-native/Libraries/NewAppScreen';
+import {NavigationContainer, useNavigation} from '@react-navigation/native';
 
-/* $FlowFixMe[missing-local-annot] The type annotation(s) required by Flow's
- * LTI update could not be added via codemod */
-const Section = ({children, title}): Node => {
-  const isDarkMode = useColorScheme() === 'dark';
+const Stack = createNativeStackNavigator();
+
+const Home = () => {
+  const nav = useNavigation();
+
   return (
-    <View style={styles.sectionContainer}>
-      <Text
-        style={[
-          styles.sectionTitle,
-          {
-            color: isDarkMode ? Colors.white : Colors.black,
-          },
-        ]}>
-        {title}
-      </Text>
-      <Text
-        style={[
-          styles.sectionDescription,
-          {
-            color: isDarkMode ? Colors.light : Colors.dark,
-          },
-        ]}>
-        {children}
-      </Text>
+    <View style={{alignItems: 'center', padding: 48}}>
+      <Text>A Home Screen</Text>
+      <Button
+        onPress={() => {
+          nav.push('Video');
+        }}
+        title="Go to the video screen"
+      />
+    </View>
+  );
+};
+
+const OtherScreen = () => {
+  return (
+    <View style={{alignItems: 'center', padding: 48}}>
+      <Text>This is another screen</Text>
+    </View>
+  );
+};
+
+const VideoScreen = () => {
+  const ref = useRef(null);
+  const nav = useNavigation();
+  const [pip, setPip] = useState(false);
+  return (
+    <View style={{alignItems: 'center', padding: 48}}>
+      <Video
+        ref={ref}
+        muted
+        source={require('./sample-5s.mp4')}
+        style={{width: 300, height: 200}}
+        pictureInPicture={pip}
+        onPictureInPictureStatusChanged={({isActive}) => setPip(isActive)}
+        onRestoreUserInterfaceForPictureInPictureStop={() => {
+          ref.current.restoreUserInterfaceForPictureInPictureStopCompleted();
+        }}
+      />
+      <Button onPress={() => setPip(!pip)} title="Picture in Picture it up" />
+      <Button onPress={() => nav.push('Other')} title="Go to another screen" />
+      <Button
+        onPress={() => nav.push('Video')}
+        title="Go to another video screen"
+      />
     </View>
   );
 };
 
 const App: () => Node = () => {
-  const isDarkMode = useColorScheme() === 'dark';
-
-  const backgroundStyle = {
-    backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
-  };
-
   return (
-    <SafeAreaView style={backgroundStyle}>
-      <StatusBar
-        barStyle={isDarkMode ? 'light-content' : 'dark-content'}
-        backgroundColor={backgroundStyle.backgroundColor}
-      />
-      <ScrollView
-        contentInsetAdjustmentBehavior="automatic"
-        style={backgroundStyle}>
-        <Header />
-        <View
-          style={{
-            backgroundColor: isDarkMode ? Colors.black : Colors.white,
-          }}>
-          <Section title="Step One">
-            Edit <Text style={styles.highlight}>App.js</Text> to change this
-            screen and then come back to see your edits.
-          </Section>
-          <Section title="See Your Changes">
-            <ReloadInstructions />
-          </Section>
-          <Section title="Debug">
-            <DebugInstructions />
-          </Section>
-          <Section title="Learn More">
-            Read the docs to discover what to do next:
-          </Section>
-          <LearnMoreLinks />
-        </View>
-      </ScrollView>
-    </SafeAreaView>
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen name="Home" component={Home} />
+        <Stack.Screen name="Video" component={VideoScreen} />
+        <Stack.Screen name="Other" component={OtherScreen} />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 };
-
-const styles = StyleSheet.create({
-  sectionContainer: {
-    marginTop: 32,
-    paddingHorizontal: 24,
-  },
-  sectionTitle: {
-    fontSize: 24,
-    fontWeight: '600',
-  },
-  sectionDescription: {
-    marginTop: 8,
-    fontSize: 18,
-    fontWeight: '400',
-  },
-  highlight: {
-    fontWeight: '700',
-  },
-});
 
 export default App;
